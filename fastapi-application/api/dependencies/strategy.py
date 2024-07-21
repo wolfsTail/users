250cc3d8642737ede9a2299df4ce30d30fce7523
DRomanov <lukaviy2la@gmail.com>
2024-07-21 14:47:35 +0300

    create get database strategy dependencies

---
+++ fastapi-application/api/dependencies/strategy.py
@@ -0,0 +1,15 @@
+from fastapi import Depends
+from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
+
+from api.dependencies import get_access_token_db
+from core.config import settings
+from core.models import AccessToken
+
+
+def get_database_strategy(
+    access_token_db: AccessTokenDatabase[AccessToken] = Depends(get_access_token_db),
+) -> DatabaseStrategy:
+    return DatabaseStrategy(
+        access_token_db,
+        lifetime_seconds=settings.access_token.lifetime_seconds
+    )
