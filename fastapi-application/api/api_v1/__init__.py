b170dc7d11a02a7b5f701d8b43cb49eabe86b2f6
DRomanov <lukaviy2la@gmail.com>
2024-07-21 15:42:08 +0300

    include auth router into apiv1router

--- fastapi-application/api/api_v1/__init__.py
+++ fastapi-application/api/api_v1/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 
 from core.config import settings
+from .auth import router as auth_router
+
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
+router.include_router(auth_router)
