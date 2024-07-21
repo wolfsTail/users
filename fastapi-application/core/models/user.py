c8cc78515120b1779b576f74483fa6bd1e4f042c
DRomanov <lukaviy2la@gmail.com>
2024-07-21 11:54:07 +0300

    add get_db method on classUser

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,8 +1,11 @@
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Base
 from core.models.mixins import IdIntMixin
 
 
 class User(Base, IdIntMixin, SQLAlchemyBaseUserTable[int]):
-    pass
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session, User)
