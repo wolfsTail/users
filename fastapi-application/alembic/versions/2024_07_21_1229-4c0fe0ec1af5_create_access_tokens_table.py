4f25423ff1f09bb167f8e36feb9b707de4dba2af
DRomanov <lukaviy2la@gmail.com>
2024-07-21 12:34:48 +0300

    add access_tokens table

---
+++ fastapi-application/alembic/versions/2024_07_21_1229-4c0fe0ec1af5_create_access_tokens_table.py
@@ -0,0 +1,53 @@
+"""create access-tokens table
+
+Revision ID: 4c0fe0ec1af5
+Revises: e95c8543e535
+Create Date: 2024-07-21 12:29:04.306372
+
+"""
+
+from typing import Sequence, Union
+
+import fastapi_users_db_sqlalchemy
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "4c0fe0ec1af5"
+down_revision: Union[str, None] = "e95c8543e535"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "access_tokens",
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("token", sa.String(length=43), nullable=False),
+        sa.Column(
+            "created_at",
+            fastapi_users_db_sqlalchemy.generics.TIMESTAMPAware(timezone=True),
+            nullable=False,
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["users.id"],
+            name=op.f("fk_access_tokens_user_id_users"),
+            ondelete="cascade",
+        ),
+        sa.PrimaryKeyConstraint("token", name=op.f("pk_access_tokens")),
+    )
+    op.create_index(
+        op.f("ix_access_tokens_created_at"),
+        "access_tokens",
+        ["created_at"],
+        unique=False,
+    )
+
+
+def downgrade() -> None:
+    op.drop_index(
+        op.f("ix_access_tokens_created_at"), table_name="access_tokens"
+    )
+    op.drop_table("access_tokens")
--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -1,10 +1,12 @@
 from .db_helper import db_helper
 from .base import Base
 from .user import User
+from .access_token import AccessToken
 
 
 __all__ = [
     "db_helper",
     "Base",
-    "User"
+    "User",
+    "AccessToken",
 ]
